4a258e091b45e43aa973d0641c3390afc1cfe5a1
Mathias <matts.neves@gmail.com>
2019-08-13 17:09:30 -0300

    initial commit

---
+++ src/pages/Login.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import './login.css';
+
+import api from '../services/api';
+import logo from '../assets/logo.svg';
+
+
+
+export default function Login({history}){
+
+    const [username, setUserName] = useState('')
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        const response = await api.post('/devs', {
+            username,
+        });
+
+        const { _id } = response.data;
+
+        history.push(`/devs/${_id}`);
+
+    }
+    
+
+    return (
+        <div className="login-container">
+            <form onSubmit={handleSubmit}>
+                <img src = {logo} alt="Tindev"/> 
+                <input 
+                    placeholder = "digite seu usuario do Github"
+                    value = {username}    
+                    onChange = {e => setUserName(e.target.value)}
+                />
+                <button type="submit">Enviar</button>
+            </form>
+
+            
+
+        </div>
+    
+    
+    );
+}
+
